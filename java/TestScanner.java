b635d3ad97551ccb3981d8004c78afdd023d8afb
Shaofeng Zhu <shaozhu@microsoft.com>
2020-07-23 23:03:54 -0700

    Test

--- java/TestScanner.java
+++ java/TestScanner.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class TestScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = a + b;
-        System.out.println(c);
+
+        int count = 0;
+        while (scanner.hasNext()) {
+            String s = scanner.next();
+            count++;
+            System.out.println(s);
+        }
+
+        System.out.println(count);
     }
 }
